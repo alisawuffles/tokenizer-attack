58316e3a38b4c3e2e7db31fb31cef1cb87c1e328
Alisa Liu <alisaliu@cs.washington.edu>
2024-05-14 16:49:11 -0700

    use tokenizers.Regex

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from tokenizers import pre_tokenizers
 from tokenizers.pre_tokenizers import Digits, ByteLevel, Metaspace, Punctuation, Split
 from tokenizers import normalizers
 from tokenizers.normalizers import NFC
+from tokenizers import Regex
 
 
 llm_normalizers = {
@@ -14,6 +15,14 @@ llm_normalizers = {
 }
 
 llm_pretokenizers = {
+    'bloom': [
+        Split(
+            pattern=Regex(" ?[^(\\s|[.,!?…。，、।۔،])]+"),
+            behavior='isolated',
+            invert=False
+        ),
+        ByteLevel(add_prefix_space=False, trim_offsets=True, use_regex=False)
+    ],
     'llama': [
         Metaspace(prepend_scheme='first'),
         Punctuation(behavior='contiguous'),
@@ -21,11 +30,10 @@ llm_pretokenizers = {
     ],
     'llama3': [
         Split(
-            pattern="(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\\r\\n\\p{L}\\p{N}]?\\p{L}+|\\p{N}{1,3}| ?[^\\s\\p{L}\\p{N}]+[\\r\\n]*|\\s*[\\r\\n]+|\\s+(?!\\S)|\\s+",
+            pattern=Regex("(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\\r\\n\\p{L}\\p{N}]?\\p{L}+|\\p{N}{1,3}| ?[^\\s\\p{L}\\p{N}]+[\\r\\n]*|\\s*[\\r\\n]+|\\s+(?!\\S)|\\s+"),
             behavior='isolated',
             invert=False
         ),
-        Digits(),
         ByteLevel(add_prefix_space=False, trim_offsets=True, use_regex=False)
     ],
     'gpt4': [
@@ -81,6 +89,7 @@ def train_tokenizer_or_dump_frequencies(text_files: str, model_name=None):
             ByteLevel(add_prefix_space=False, trim_offsets=True, use_regex=True)
         ])
     else:
+        print(f'Using config of {model_name}')
         if model_name in llm_normalizers:
             tokenizer.normalizer = normalizers.Sequence(llm_normalizers[model_name])
         if model_name in llm_pretokenizers:
@@ -89,3 +98,25 @@ def train_tokenizer_or_dump_frequencies(text_files: str, model_name=None):
     tokenizer.train(text_files, trainer)
 
     return tokenizer
+
+
+def is_valid_unicode(data):
+    try:
+        data.decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
+
+
+def truncate_file(filename, wanted_filesize):
+    with open(filename, 'rb') as f:
+        f.seek(wanted_filesize)
+        data = f.read(1)
+        while data and not is_valid_unicode(data):
+            data = f.read(1)
+            wanted_filesize += 1
+    with open(filename, 'r+', encoding='utf-8') as fin:
+        fin.truncate(wanted_filesize)
+
+    # if we ever need to rerun all our experiments (god forbid), we should return the actual filesized used and record that instead
+    # return wanted_filesize
